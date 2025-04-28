955daa5c6019f4c9f238017959ef8cc8e306ab0a
Anchiraico-Aylas <60933304@continental.edu.pe>
2025-04-28 00:12:03 -0500

    Temp Tracker v1.0

---
+++ api-test.js
@@ -0,0 +1,10 @@
+const claveApi = 'fd25cada1e7b47678b044332252804';
+const idioma = 'es';
+const ciudad = 'Huancayo';
+
+const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+
+const response = await fetch(apiClimaActual);
+let data = await response.json();
+
+console.log(data.current.condition);
